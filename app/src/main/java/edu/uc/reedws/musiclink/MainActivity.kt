86fec39fd4b8f0d93d26e712f1fd8b30abfc429e
Benjamin Romig <romigbl@mail.uc.edu>
2020-06-17 00:09:18 -0400

    Search Button now links to search screen

--- app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.uc.reedws.musiclink
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.uc.reedws.musiclink.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.mainScreen, MainFragment.newInstance())
                     .commitNow()
+        }
+        searchButton.setOnClickListener {
+            val intent = Intent(this,SearchActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/edu/uc/reedws/musiclink/SearchActivity.kt
@@ -0,0 +1,11 @@
+package edu.uc.reedws.musiclink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_screen)
+    }
+}
