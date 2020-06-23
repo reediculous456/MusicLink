8ae6bd8897c68e07a3f1368ff3e4ff2a0f7e2054
Benjamin Romig <romigbl@mail.uc.edu>
2020-06-23 14:39:10 -0400

    Mock data of playlists

--- app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.uc.reedws.musiclink
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import edu.uc.reedws.musiclink.ui.main.MainFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -11,6 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        title = "Playlist Library"
+        val arrayAdapter: ArrayAdapter<*>
+        val playLists = arrayOf(
+            "Rock",
+            "Pop",
+            "Jazz",
+            "Indie",
+            "Metal",
+            "Classical"
+        )
+        val listView = findViewById<ListView>(R.id.listOfPlayLists)
+        arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, playLists
+        )
+        listView.adapter = arrayAdapter
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.mainScreen, MainFragment.newInstance())
