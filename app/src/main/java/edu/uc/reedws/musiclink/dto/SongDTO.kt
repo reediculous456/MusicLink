b67da3ed7ee6dc9a06601ac0ceab5a52b72b3eda
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-06-29 18:35:35 -0400

    Added toString() function inside DTO class

--- app/src/main/java/edu/uc/reedws/musiclink/dto/SongDTO.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/dto/SongDTO.kt
@@ -1,3 +1,7 @@
 package edu.uc.reedws.musiclink.dto
 
-data class SongDTO(var title: String, var album: String, var artist: String)
+data class SongDTO(var title: String, var album: String, var artist: String) {
+    override fun toString(): String {
+        return "$title $album $artist"
+    }
+}
