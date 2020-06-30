d221e26cfda88e075bb911cf2842436b32332082
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-06-29 23:54:27 -0400

    Reverted room database runtime version and cleaned up the code a little bit

--- app/src/main/java/edu/uc/reedws/musiclink/dto/SongDTO.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/dto/SongDTO.kt
@@ -1,7 +1,5 @@
 package edu.uc.reedws.musiclink.dto
 
 data class SongDTO(var title: String, var album: String, var artist: String) {
-    override fun toString(): String {
-        return "$title $album $artist"
-    }
+    override fun toString() = "$title $album $artist"
 }
