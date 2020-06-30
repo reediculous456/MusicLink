b023055110b3479150d981333ae8af88a1ec772c
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-06-29 23:34:17 -0400

    Added a indices to speed up the query

--- app/src/main/java/edu/uc/reedws/musiclink/dto/PlaylistDTO.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/dto/PlaylistDTO.kt
@@ -1,9 +1,10 @@
 package edu.uc.reedws.musiclink.dto
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName="playlists")
-data class PlaylistDTO(var name: String = "", var Songs: String = "", var ownerId: String = "", @PrimaryKey(autoGenerate = true) var id: Int = 0) {
+@Entity(tableName="playlists", indices = [Index("name"), Index("song"), Index("ownerId"), Index("id")])
+data class PlaylistDTO(var name: String = "", var songs: String = "", var ownerId: String = "", @PrimaryKey(autoGenerate = true) var id: Int = 0) {
     override fun toString() = name
 }
