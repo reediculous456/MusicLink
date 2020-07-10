8fab1a4ca02abd439baf1eb2506c8892f877a573
Benjamin Romig <romigbl@mail.uc.edu>
2020-07-10 04:24:29 -0400

    Bottom Navigation Bar now works, changed from individual listeners

--- app/src/main/java/edu/uc/reedws/musiclink/SearchActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/SearchActivity.kt
@@ -1,19 +1,22 @@
 package edu.uc.reedws.musiclink
-
+//
+//import android.content.Intent
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
+//import kotlinx.android.synthetic.main.main_activity.*
+
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.search_screen)
+        setContentView(R.layout.search_view)
 
-        // Opens the Main or Playlist Library Screen
-        libraryButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        /** Opens the Main or Playlist Library Screen */
+//        libraryButton.setOnClickListener {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
