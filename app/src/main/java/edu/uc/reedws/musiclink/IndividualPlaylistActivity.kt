590661069cccfc8e65bf49527ea6ff7f4c5c29c9
Benjamin Romig <romigbl@mail.uc.edu>
2020-07-10 04:24:29 -0400

    Bottom Navigation Bar now works, changed from individual listeners

--- app/src/main/java/edu/uc/reedws/musiclink/IndividualPlaylistActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/IndividualPlaylistActivity.kt
@@ -8,19 +8,48 @@ import kotlinx.android.synthetic.main.main_activity.*
 class IndividualPlaylistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.playlistsongview)
+        setContentView(R.layout.playlist_song_view)
 
-        // Opens the Main or Playlist Library Screen
-        libraryButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        /** Share functionality */
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "type/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "This is example text.")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Example Subject")
+            startActivity(Intent.createChooser(intent, "Share playlist"))
         }
-        // Opens Search Screen
-        searchButton.setOnClickListener {
-            val intent = Intent(this,SearchActivity::class.java)
-            startActivity(intent)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.search_menu -> {
+                    val intent = Intent(this,SearchActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.library_menu -> {
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.profile_menu -> {
+                    true
+                }
+                R.id.settings_menu -> {
+                    true
+                }
+                else -> false
+            }
         }
-        // Opens Search Screen and will need to associate with the playlist to avoid having to select a playlist
+        /** Opens the Main or Playlist Library Screen */
+//        libraryButton.setOnClickListener {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+        /** Opens Search Screen */
+//        searchButton.setOnClickListener {
+//            val intent = Intent(this,SearchActivity::class.java)
+//            startActivity(intent)
+//        }
+        /** Opens Search Screen and will need to associate with the playlist to avoid having to select a playlist */
         addPlaylistOrSongButton.setOnClickListener {
             val intent = Intent(this,SearchActivity::class.java) // Change this later to include playlist data without making you select a playlist
             startActivity(intent)
--- app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/MainActivity.kt
@@ -2,59 +2,104 @@ package edu.uc.reedws.musiclink
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+//import androidx.lifecycle.Observer
+//import androidx.lifecycle.ViewModelProvider
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import edu.uc.reedws.musiclink.ui.main.ApplicationViewModel
-import edu.uc.reedws.musiclink.ui.main.MainFragment
+//import edu.uc.reedws.musiclink.ui.main.MainFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity() : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: ApplicationViewModel
+
+//    private lateinit var recyclerView: RecyclerView
+//    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+//    private lateinit var viewManager: RecyclerView.LayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        viewModel = ViewModelProvider(this).get(ApplicationViewModel::class.java);
 
-        val listView = findViewById<ListView>(R.id.listOfPlayLists)
+//        viewManager = LinearLayoutManager(this)
+//        //viewAdapter = MyAdapter(DataSet)
+//
+//        recyclerView = findViewById<RecyclerView>(R.id.rcycListOfPlayLists).apply {
+//            setHasFixedSize(true)
+//            layoutManager = viewManager
+//            //adapter = viewAdapter
+//        }
 
-        viewModel.playlists.observe(this, Observer {
-            playLists -> listView.adapter = ArrayAdapter(
-                this,
-                android.R.layout.simple_list_item_1, playLists
-            )
-        })
+//        viewModel = ViewModelProvider(this).get(ApplicationViewModel::class.java);
+//
+//        val listView = findViewById<ListView>(R.id.listOfPlayLists)
+//
+//        viewModel.playlists.observe(this, Observer {
+//            playLists -> listView.adapter = ArrayAdapter(
+//                this,
+//                android.R.layout.simple_list_item_1, playLists
+//            )
+//        })
+//
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                    .replace(R.id.mainScreen, MainFragment.newInstance())
+//                    .commitNow()
+//        }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.mainScreen, MainFragment.newInstance())
-                    .commitNow()
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.search_menu -> {
+                    val intent = Intent(this,SearchActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.library_menu -> {
+//                    val intent = Intent(this,MainActivity::class.java)
+//                    startActivity(intent)
+                    true
+                }
+                R.id.profile_menu -> {
+                    true
+                }
+                R.id.settings_menu -> {
+                    true
+                }
+                else -> false
+            }
         }
-        // Opens Search Screen
-        searchButton.setOnClickListener {
-            val intent = Intent(this,SearchActivity::class.java)
-            startActivity(intent)
+
+        /** Share functionality */
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "type/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "This is example text.")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Example Subject")
+            startActivity(Intent.createChooser(intent, "Share playlist"))
         }
-        // Opens Dialog Screen to add a playlist
+        /** Opens Search Screen */
+//        searchButton.setOnClickListener {
+//            val intent = Intent(this,SearchActivity::class.java)
+//            startActivity(intent)
+//        }
+        /** Opens Dialog Screen to add a playlist */
         val addPlaylistDialogBtn = findViewById<FloatingActionButton>(R.id.addPlaylistOrSongButton)
         addPlaylistDialogBtn.setOnClickListener {
             showAlertDialog(viewModel)
         }
-        // Opens the Main or Playlist Library Screen
-        libraryButton.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }
+        /** Opens the Main or Playlist Library Screen */
+//        libraryButton.setOnClickListener {
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 
-    // Method to show Dialog Screen for adding a new playlist
+    /** Show Dialog Screen for adding a new playlist */
     private fun showAlertDialog(viewModel: ApplicationViewModel) {
 
         val addPlaylistDialogBuilder = AlertDialog.Builder(this)
@@ -63,12 +108,11 @@ class MainActivity() : AppCompatActivity() {
         val dialogLayout = inflater.inflate(R.layout.add_playlist_dialog, null)
         val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
         addPlaylistDialogBuilder.setView(dialogLayout)
-        addPlaylistDialogBuilder.setPositiveButton("Done") { dialogInterface, i -> Toast.makeText(applicationContext, "You added " + editText.text.toString(), Toast.LENGTH_SHORT).show()
+        addPlaylistDialogBuilder.setPositiveButton("Done") { _, _ -> Toast.makeText(applicationContext, "You added " + editText.text.toString(), Toast.LENGTH_SHORT).show()
             val newPlaylistName = editText.text
             viewModel.createPlaylist(newPlaylistName.toString())
         }
         addPlaylistDialogBuilder.setNeutralButton("Cancel") { dialog, id -> dialog.cancel()}
-
         addPlaylistDialogBuilder.show()
     }
 }
