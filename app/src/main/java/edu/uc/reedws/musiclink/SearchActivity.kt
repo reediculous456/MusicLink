89a3fd5b19973b0e75058571ed0304dd8e9de98f
Wesley Reed <reed.wesley.s@gmail.com>
2020-07-14 21:05:44 -0400

    code cleanup

--- app/src/main/java/edu/uc/reedws/musiclink/SearchActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/SearchActivity.kt
@@ -1,12 +1,11 @@
 package edu.uc.reedws.musiclink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_view)
-
     }
 }
--- app/src/main/java/edu/uc/reedws/musiclink/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/ui/main/MainFragment.kt
@@ -1,18 +1,20 @@
 package edu.uc.reedws.musiclink.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import edu.uc.reedws.musiclink.R
 
 class MainFragment : Fragment() {
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
