a30154ebb99cef44905e777bb113deafeb8a628f
allieprusha <prushaar@mail.uc.edu>
2020-07-26 15:52:37 -0400

    Minor change - just a recommendation. See code review comments for more information :)

--- app/src/main/java/edu/uc/reedws/musiclink/IndividualPlaylistActivity.kt
+++ app/src/main/java/edu/uc/reedws/musiclink/IndividualPlaylistActivity.kt
@@ -19,8 +19,8 @@ class IndividualPlaylistActivity : AppCompatActivity() {
         shareButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "type/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, "This is example text.")
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Example Subject")
+            intent.putExtra(Intent.EXTRA_TEXT, "One of your friends shared a playlist with you through MusicLink!")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "A friend shared a playlist using MusicLink!")
             startActivity(Intent.createChooser(intent, "Share playlist"))
         }
 
